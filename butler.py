3f75cd63eec6a0cde736615cd84e2129fb83016d
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-08-20 18:41:16 +0200

    More fun with GitPython

--- butler.py
+++ butler.py
@@ -37,6 +37,7 @@ def display_git_info():
     else:
         print("Directory is a git repository, obtaining info...")
         repo = Repo('.')
+        print("Active branch: %s" % repo.active_branch)
         if repo.is_dirty(): print("Repository is dirty!")
         if not repo.untracked_files:
             print("No untracked files")
@@ -50,6 +51,24 @@ def display_git_info():
             print("Submodules:")
             for _ in repo.submodules: 
                 print("  %s" % _)
+                if _.module_exists(): 
+                    print("    Module is available")
+                    sm = _.module()
+                    print("    Working tree directory: %s" % sm.working_tree_dir)
+                    print("    Branches: %s" % ', '.join([
+                        ('*' if _ == sm.active_branch else '') + str(_) for _ in sm.branches]))
+                    #print("      Active branch: %s" % sm.active_branch)
+                    #for br in sm.branches:
+                    #    print("      %s" % br)
+                else:
+                    print("    Module is NOT available")
+        if not any(_ for _ in repo.index.entries.items() if _[0][1] != 0):
+            print("No uncommitted changes")
+        else:
+            print("Index:")
+            for (path, stage), entry in repo.index.entries.items():
+                if stage != 0:
+                    print("  %s: %s" % (path, stage))
     
     
 # The main routine
