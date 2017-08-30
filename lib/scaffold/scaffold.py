55eb2f67fb32eb576363aa78df12e6e52dac41a8
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-08-30 19:17:16 +0200

    Scaffold: warnings

--- lib/scaffold/scaffold.py
+++ lib/scaffold/scaffold.py
@@ -1,14 +1,6 @@
 import re
 
 
-#_splitter = re.compile(r'^\s*#\$\:\s*(\w+)\s*(?:\:(.*))?$((?:\n.*$)*)\n^\s*#\$/\s*(\1)(?:.*)$', re.MULTILINE)
-#        
-#def _scan_string(s):
-#    for m in _splitter.finditer(s):
-#        print('--------------------------------')
-#        print(m.group(3))
-
-
 class Node:
     def __init__(self, tag = None, content = b'', line_count = 0):
         self._tag = tag
@@ -17,12 +9,12 @@ class Node:
         self.children = []
         self._sealed = False
         
-    def append(self, content, line_count):
+    def append_content(self, content, line_count):
         assert not (len(content) == 0) and line_count != 0
         if self.children:
             if self.children[-1].is_sealed():
                 self.children.append( Node() )
-            self.children[-1].append(content, line_count)
+            self.children[-1].append_content(content, line_count)
         else:
             self._content += content
             self._line_count += line_count
@@ -93,7 +85,9 @@ class Scaffold:
         
         curr_branch = [Node()]
         indents = []
+        warnings = []
         
+        line_num = 1
         for line_buf in stream:
             line = line_buf.decode(encoding).rstrip()
             indent = len(line) - len(line.lstrip())
@@ -102,22 +96,25 @@ class Scaffold:
                 # Element openers and closers become part of the *containing* node (for now)
                 if line[2] == ':':
                     indents.append(indent)
-                    curr_branch[-1].append(line_buf, 1)
+                    curr_branch[-1].append_content(line_buf, 1)
                     child = curr_branch[-1].create_new_branch(line[3:].strip())
                     curr_branch.append(child)
                 elif line[2] == '/':
                     curr_branch[-1].seal()
                     curr_branch.pop()
                     if indent != indents[-1]:
-                        raise Exception('opening/closing tag indent mismatch (opening: %d, closing %d)' % (indents[-1], indent))
+                        #raise Exception('opening/closing tag indent mismatch (opening: %d, closing %d)' % (indents[-1], indent))
+                        msg = 'opening/closing tag indent mismatch (opening: %d, closing %d)' % (indents[-1], indent)
+                        warnings.append(((line_num, indent), msg))
                     indents.pop()
-                    curr_branch[-1].append(line_buf, 1)
+                    curr_branch[-1].append_content(line_buf, 1)
             else:
-                curr_branch[-1].append(line_buf, 1)
+                curr_branch[-1].append_content(line_buf, 1)
+            line_num += 1
         #else:
         #    print('EOF, total byte_size:', byte_offs)
                 
         sc.root_node = curr_branch[0]
         sc.root_node.seal()
         
-        return sc
+        return sc, warnings
--- lib/scaffold/test_scaffold.py
+++ lib/scaffold/test_scaffold.py
@@ -6,26 +6,26 @@ from . import Scaffold
 
 class TestScaffoldClass(unittest.TestCase):
     
-    def test_scan_stream(self):
+    def test_basic_parsing(self):
     
-        bytes = pkg_resources.resource_string(__name__, 'testdata/CMakeLists.txt')
+        bytes = pkg_resources.resource_string(__name__, 'testdata/cmake1.txt')
         file_size  = len(bytes)
-        rs = pkg_resources.resource_stream(__name__, 'testdata/CMakeLists.txt')
+        rs = pkg_resources.resource_stream(__name__, 'testdata/cmake1.txt')
         line_count = len([_ for _ in rs])
         rs.close()
-        print("File size in bytes:", file_size)
-        print("File line count   :", line_count)
+        #print("File size in bytes:", file_size)
+        #print("File line count   :", line_count)
         
-        bs = pkg_resources.resource_stream(__name__, 'testdata/CMakeLists.txt')
-        scaffold = Scaffold.from_byte_stream(bs)
-        bs.close()
+        rs = pkg_resources.resource_stream(__name__, 'testdata/cmake1.txt')
+        scaffold, warnings = Scaffold.from_byte_stream(rs)
+        rs.close()
         
         root_node = scaffold.root_node
         self.assertTrue(root_node.depth() == 2)
         
-        print('Root node byte size  :', root_node.byte_size ())
-        print('Root node line count :', root_node.line_count())
-        print('Root node child count:', len(root_node.children))
+        #print('Root node byte size  :', root_node.byte_size ())
+        #print('Root node line count :', root_node.line_count())
+        #print('Root node child count:', len(root_node.children))
         self.assertEqual(root_node.byte_size (), file_size)
         self.assertEqual(root_node.line_count(), line_count)
         self.assertEqual(len(root_node.children), 3)
@@ -34,5 +34,20 @@ class TestScaffoldClass(unittest.TestCase):
         self.assertEqual(len(tagged_nodes), 1)
         self.assertEqual(tagged_nodes[0].tag(), 'main-target')
         
-        content = tagged_nodes[0].content()
-        self.assertTrue('add_executable' in content.decode('utf-8'))
+        content = tagged_nodes[0].content().decode('utf-8')
+        self.assertTrue('add_executable' in content)
+        
+    def test_warnings(self):
+
+        rs = pkg_resources.resource_stream(__name__, 'testdata/cmake2.txt')
+        scaffold, warnings = Scaffold.from_byte_stream(rs)
+        rs.close()
+        
+        self.assertEqual(len(warnings), 1)
+        warning = warnings[0]
+        line_num, col_num = warning[0]
+        message = warning[1]
+        self.assertEqual(line_num, 19)
+        self.assertEqual(col_num, 4)
+        self.assertIn('indent mismatch', message)
+        
